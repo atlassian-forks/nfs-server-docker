221b2be80ad1de31fceddd212ac5fb6645ed0ad9
Wolfgang Kritzinger <wkritzinger@atlassian.com>
2021-07-19 13:30:24 +1000

    DCKUBE-436 Simplify building docker image.

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This image should not be used for production purposes. ⚠️
 # Upstream
 This source repo was originally based on:
 https://github.com/GoogleCloudPlatform/nfs-server-docker
+
+# How to build
+
+    docker build -t atlassian/nfs-server-test .
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+# Copyright 2016 The Kubernetes Authors.
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -euo pipefail
 
@@ -81,7 +95,7 @@ function stop()
     kill $( pidof rpcbind ) 2>/dev/null || true
 }
 
-trap stop EXIT TERM INT
+trap stop TERM INT HUP USR1 USR2
 start "$@"
 
-tail -f /dev/null
+tail -f /dev/null & wait $!
